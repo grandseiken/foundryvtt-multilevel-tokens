637d263f2ccd9d696050af465b377b89c42806b4
stu <s+github@stu.scot>
2020-06-14 21:23:02 +0100

    update

--- multilevel.js
+++ multilevel.js
@@ -1,11 +1,14 @@
 class MultilevelTokens {
   constructor() {
     Hooks.on("updateToken", this._onUpdateToken.bind(this));
+    console.log("Multilevel Tokens | Initialized");
   }
 
   _onUpdateToken(scene, embedded, update, options, userId) {
-    console.log(scene.toString() + ": " + update.toString());
+    console.log(userId);
+    console.log(update);
   }
 }
 
+console.log("Multilevel Tokens | Loaded");
 Hooks.on('init', () => game.multilevelTokens = new MultilevelTokens())
